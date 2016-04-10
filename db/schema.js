d0f0748a5336d4af7b28fe0b79d8a7887a97a454
Anissa Jones <anissajones10@gmail.com>
2016-04-10 19:35:24 -0400

    Adds Schema for Restaurants and Menus.

--- db/schema.js
+++ db/schema.js
@@ -5,3 +5,21 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
   console.log("Connected to MongoDB!");
 });
+
+var Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId;
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelp_url: String,
+  items: []
+});
+
+var MenuSchema = new Schema({
+  title: String,
+  restaurants : [RestaurantSchema]
+});
