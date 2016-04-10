f26b8000664f31863873a079e86b72adb5d0dd15
Anissa Jones <anissajones10@gmail.com>
2016-04-10 19:18:12 -0400

    installs Mongoose and uses db.once to console log a message.

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,6 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log("Connected to MongoDB!");
+});
