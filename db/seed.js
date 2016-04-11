a8691ea6c9a40cffe09012310ff589dca0227906
Anissa Jones <anissajones10@gmail.com>
2016-04-11 08:14:57 -0400

    Creates a new restaurant and menu.

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,34 @@
-var Schema = require("../db/schema.js");
+var mongoose = require('mongoose');
+var Schema = require("./schema.js");
+
+var RestaurantModel = Schema.RestaurantModel;
+var MenuModel = Schema.MenuModel;
+
+RestaurantModel.remove({}, function(err){
+  console.log(err);
+});
+MenuModel.remove({}, function(err){
+  console.log(err);
+});
+
+var lobstershack= new RestaurantModel({
+  name: "Lobster Shack",
+  address: {
+    street: "5555 Shell Fish Ln",
+    zipcode: 20005
+  },
+  yelp_url: "http://www.yelp.com/biz/lobster-shack-restaurant"
+});
+
+var menu1 = new MenuModel({title: "Breakfast menu"});
+
+//FIXME: what gets pushed into what?
+
+lobstershack.menus.push(menu1);
+lobstershack.save(function(err, restaurant){
+  if(err){
+    console.log(err);
+  }else{
+    console.log(restaurant + " was saved to our db!");
+  }
+});
