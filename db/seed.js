86719def45db9acf3f711703fd27990965caea0a
Anissa Jones <anissajones10@gmail.com>
2016-04-12 22:20:39 -0400

    Adds another restaurant.

--- db/seed.js
+++ db/seed.js
@@ -31,3 +31,20 @@ lobstershack.save(function(err, restaurant){
     console.log(restaurant + " was saved to our db!");
   }
 });
+
+var noodles= new RestaurantModel({
+  name: "Noodles Yum Yum",
+  address: {
+    street: "1234 Main Noodle Rd",
+    zipcode: 20005
+  },
+  yelp_url: "http://www.yelp.com/biz/noodles-so-yummy"
+});
+
+noodles.save(function(err, restaurant){
+  if(err){
+    console.log(err);
+  }else{
+    console.log(restaurant + " was saved to our db!");
+  }
+});
