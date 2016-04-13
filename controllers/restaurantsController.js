6a5816837188786e830de4b31c12f930ce61e53a
Anissa Jones <anissajones10@gmail.com>
2016-04-12 22:14:34 -0400

    Adds a update method.

--- controllers/restaurantsController.js
+++ controllers/restaurantsController.js
@@ -9,6 +9,15 @@ var restaurantsController = {
       console.log(docs);
     });
   },
+  update: function(req, update){
+    RestaurantModel.findOneAndUpdate({name: req.name}, {name: update.name}, {new: true}, function(err, docs){
+      if(err){
+        console.log(err)
+      }else{
+        console.log(docs);
+      }
+    });
+  },
   show: function(req){
     RestaurantModel.findOne({"name": req.name}, function(err, docs){
       console.log(docs);
@@ -19,3 +28,4 @@ var restaurantsController = {
 restaurantsController.index();
 restaurantsController.show({name: "Lobster Shack"});
 restaurantsController.show({zipcode: 20005});
+restaurantsController.update({name: "Lobster Shack"}, {name: "Lobster Cuisine"});
